d56136cc369b850c71ab208d0d4ccf97459f55c3
Eduardo de O. Santos <75816516+Eduardo-S4ntos@users.noreply.github.com>
2022-08-25 16:39:26 -0300

    correções de bug

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,7 +14,7 @@ function switchTheme() {
     const music = theme === 'light-theme' ? 'normal-world.mpeg' : 'inverted-world.mpeg'
 
     const audio = document.getElementById('music');
-    audio.src = `assets/musics/${music}`;
+    audio.src = `assets/music/${music}`;
     audio.play();
     audio.volume = 0.2;
 }
